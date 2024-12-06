aef5685958b476d0d073a5427f99939d81a57fb2
PeterSadek <Peter_Sadek@outlook.com>
2024-12-06 23:45:42 +0200

    Adjust file system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Fragment, useState } from "react";
-import Header from "./Layout/Header";
-import Meals from "./Meals/Meals";
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 import Cart from "./Cart/Cart";
 import CartProvider from "./store/CartProvider";
 
--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,4 +1,4 @@
-import Modal from "../UI/Modal";
+import Modal from "../components/UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
